59709ef788fc9647dc2a8122afbf0e0b28cc9c08
aristo <dkfud2121@naver.com>
2023-02-09 15:33:43 +0900

    feat : current status, 2nd section

--- demo/src/main/java/com/example/demo/persistence/TodoRepository.java
+++ demo/src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -1,13 +1,12 @@
 package com.example.demo.persistence;
 
-import com.example.demo.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.demo.model.TodoEntity;
+
+import java.util.List;
 
 @Repository
-public interface TodoRepository extends JpaRepository<TodoEntity, String> {
-    List<TodoEntity> findByUserId(String userId);
-    // ?1는 메서드의 매개변수의 순서 위치
-    @Query("select * from todo t  where t.userId = ?1")
+public interface TodoRepository extends JpaRepository<TodoEntity, String>{
     List<TodoEntity> findByUserId(String userId);
 }
