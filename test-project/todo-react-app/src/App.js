1d2ff4cfb4e40b481afc36e400e6afbec6cd1717
aristo <dkfud2121@naver.com>
2023-02-15 00:15:07 +0900

    feat : add style todo component

--- test-project/todo-react-app/src/App.js
+++ test-project/todo-react-app/src/App.js
@@ -1,21 +1,31 @@
 import Todo from "./Todo";
 import "./App.css";
 import React from "react";
+import { Paper, List } from "@material-ui/core";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: { id: 0, title: "hello world 1", done: true },
+      items: [
+        { id: "0", title: "hello world 1", done: true },
+        { id: "1", title: "hello world 2", done: false },
+      ],
     };
   }
 
   render() {
-    return (
-      <div className="App">
-        <Todo item={this.state.item} />
-      </div>
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} />
+          ))}
+        </List>
+      </Paper>
     );
+
+    return <div className="App">{todoItems}</div>;
   }
 }
 
