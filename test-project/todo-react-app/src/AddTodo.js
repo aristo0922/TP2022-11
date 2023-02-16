7caf2180f69abfef86a5a61ddd646a253bfa126b
aristo <dkfud2121@naver.com>
2023-02-16 19:18:18 +0900

    feat : add AddTodo.js

--- test-project/todo-react-app/src/AddTodo.js
+++ test-project/todo-react-app/src/AddTodo.js
@@ -7,15 +7,30 @@ class AddTodo extends React.Component {
     this.state = { item: { title: "" } }; //사용자 입력을 저장할 오브젝트
   }
 
+  onInputChange = (e) => {
+    const thisItem = this.state.item;
+    thisItem.title = e.target.value;
+    this.setState({item: thisItem});
+    console.log(thisItem);
+  }
   render() {
     return (
       <Paper style={{ margin: 16, padding: 16 }}>
         <Grid container>
           <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-            <TextField placeholder="Add Todo Here" fullWidth />
+            <TextField 
+             placeholder="Add Todo Here"
+             fullWidth
+             onChange = {this.onInputChange}
+             value={this.state.item.title} />
           </Grid>
-          <Grid fullWidth color="secondary" variant="outlined">
+          <Grid xs={1} md={1} item>
+            <Button 
+            fullWidth
+            color="secondary"
+            variant="outlined">
             +
+            </Button>
           </Grid>
         </Grid>
       </Paper>
