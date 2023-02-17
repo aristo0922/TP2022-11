843995e4b58e4af60207efd9b454c69b15f3deec
aristo <dkfud2121@naver.com>
2023-02-18 02:15:23 +0900

    feat : add deleteEventHandler

--- test-project/todo-react-app/src/App.js
+++ test-project/todo-react-app/src/App.js
@@ -8,19 +8,34 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [
-        { id: "0", title: "hello world 1", done: true },
-        { id: "1", title: "hello world 2", done: false },
-      ],
+      items: [],
     };
   }
 
+  // 1. 함수 추
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;
+    item.done = false;
+    thisItems.push(item);
+    this.setState({ items: thisItems });
+    console.log("items : ", this.state.items);
+  };
+
+  delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before Update Items : ", this.state.items);
+    const newItems = thisItems.filter((e) => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      console.log("Update Items : ", this.state.items);
+    });
+  };
   render() {
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} />
+            <Todo item={item} key={item.id} delete={this.delete} />
           ))}
         </List>
       </Paper>
@@ -29,7 +44,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Container maxWidth="md">
-          <AddTodo />
+          <AddTodo add={this.add} />
           <div className="TodoList">{todoItems}</div>
         </Container>
       </div>
